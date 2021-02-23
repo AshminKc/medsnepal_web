b813b98444c54dffea30ee0c20c1dc88f6a13100
AshminKc <ashminkc27@gmail.com>
2021-02-23 19:48:55 +0545

    changes in product route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -66,6 +66,7 @@ router.put('/:id', isAuth, isAdmin, async (req, res) => {
     product.countInStock = req.body.countInStock;
     product.description = req.body.description;
     const updatedProduct = await product.save();
+    
     if (updatedProduct) {
       return res
         .status(200)
